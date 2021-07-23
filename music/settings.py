c903c7bb45ba5c3213a9009499b77d613de38f01
Tejender Dubey <tejenderdubey102117@gmail.com>
2021-07-23 18:15:13 +0530

    settings changed

--- music/settings.py
+++ music/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_URL=os.path.join(BASE_DIR, 'musicbeats/static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
